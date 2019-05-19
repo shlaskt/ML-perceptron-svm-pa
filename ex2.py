7e33717343cfea8b826502cad619631747b0aec4
shlaskt <shlaski@gmail.com>
2019-05-19 15:50:11 +0300

    adding comments

--- ex2.py
+++ ex2.py
@@ -1,29 +1,35 @@
+# Tal levy 312497910
+# Tomer Shlasky 204300602
+
 import sys
 from enum import Enum
 import random
 import numpy as np
 
-# enum for the sex
+
+# enum for the Gender
 class Sex(Enum):
     INFANT = 0.6
     FEMALE = 0.4
     MALE = 0.2
 
 
-class Group(Enum):
-    FIRST = 0
-    SECOND = 1
-    THIRD = 2
-
-
+# enum for the data
 class Data(Enum):
     CLUSTERS = 3
     FEATCHERS = 8
 
 
+"""
+parser- split the test_x and the train_x by ","
+replace the sex featcher with numeric values
+takes the values from the file and write it to matrix
+"""
+
+
 def parser(file):
     data_sets = []
-    f = open(file, "r+")
+    f = open(file, "r+")  # open file for reading
     for line in f:
         if "M" in line:
             local_line = line.replace("M", str(Sex.MALE.value))
@@ -37,21 +43,31 @@ def parser(file):
     return data_sets
 
 
+# calculate y hat
 def get_y_hat(w, x):
     return np.argmax(np.dot(w, x))
 
 
+# calculate the loss function
 def get_loss(w, x, y):
     y_hat = get_y_hat(w, x)
     return max(0, 1 - np.dot(w[y], x) + np.dot(w[y_hat], x))
 
 
+# calculate tao for the pa algorithm
 def calc_tau(w, x, y):
-    # TODO check ord
     norm = 2 * np.power(np.linalg.norm(x, ord=2), 2)
     if norm == 0:
         norm = 1  # avoid from divide by 0
     return get_loss(w, x, y) / norm
+
+
+"""
+generic update - for the pa and the pa.
+checks if y != y_hat, if it is changes w (weights matrix)
+by changing y and y hat columns
+getting const - eta ot tau
+"""
 
 
 def generic_update(w, y, y_hat, x, const):
@@ -61,11 +77,25 @@ def generic_update(w, y, y_hat, x, const):
     return w
 
 
+"""
+pa train-
+calc y hat and tao.
+if y != y hat do the update.
+"""
+
+
 def pa_train_update(w, y, x):
     y_hat = get_y_hat(w, x)
     tau = calc_tau(w, x, y)
     generic_update(w, y, y_hat, x, tau)
     return w
+
+
+"""
+perceptron train-
+calc y hat.
+if y != y hat do the update.
+"""
 
 
 def perceptron_train_update(w, y, x, eta):
@@ -74,7 +104,14 @@ def perceptron_train_update(w, y, x, eta):
     return w
 
 
-# TODO check - id update in the else also
+"""
+svm update
+calc y hat and lambda
+if y != y hat- change w by changing y and y hat columns
+update all other columns
+"""
+
+
 def svm_train_update(w, y, x, eta):
     y_hat = get_y_hat(w, x)
     lamda = 0.5
@@ -92,7 +129,7 @@ def svm_train_update(w, y, x, eta):
     return w
 
 
-# initialize 3 zeros matrixs
+# initialize 3 zeros matrix
 def init_w_for_algs():
     w_1 = np.zeros([Data.CLUSTERS.value, Data.FEATCHERS.value])
     w_2 = np.zeros([Data.CLUSTERS.value, Data.FEATCHERS.value])
@@ -100,12 +137,22 @@ def init_w_for_algs():
     return w_1, w_2, w_3
 
 
+"""
+train all 3 algorithms - perceptron, svm and pa
+init 3 0 matrix- one for every algorithm and pick constants- eta and number of iterations
+in every iteration- shuffle the data and go over x (data) and y (labels)
+every 1000 samples on the data - change the constants
+train the 3 algorithms (calc y hat, check if y!=y hat and if it is change w)
+returns the final three w-s.
+"""
+
+
 def train(x, y):
     w_perceptron, w_svm, w_pa = init_w_for_algs()
     eta_per = 1
     eta_svm = 1.5
     iterations = 20
-    change_ethas = 0
+    change_ethas = 0  # change every 1000 samples
     for i in range(iterations):
         # shuffle the data
         x, y = shuffle_data(x, y)
@@ -121,6 +168,12 @@ def train(x, y):
     return w_perceptron, w_svm, w_pa
 
 
+"""
+classify (after training)
+calc y hat for every sample- and display it gor every algorithm.
+"""
+
+
 def classify(w_per, w_svm, w_pa, x):
     for line in x:
         # get the prediction for every alg and print
@@ -130,11 +183,23 @@ def classify(w_per, w_svm, w_pa, x):
         print("perceptron: " + str(y_hat_per) + ", " + "svm: " + str(y_hat_svm) + ", " + "pa: " + str(y_hat_pa))
 
 
+"""
+shuffle the data
+zip x and y, shuffle and unzip them
+"""
+
+
 def shuffle_data(x, y):
     zip_x_y = list(zip(x, y))
     random.shuffle(zip_x_y)
     new_x, new_y = zip(*zip_x_y)
     return new_x, new_y
+
+
+"""
+normalize the data by columns (for every featcher)
+do the min- max normalized algorithm.
+"""
 
 
 def normal_data(data):
@@ -149,6 +214,12 @@ def normal_data(data):
     return normal.transpose()  # back to the original
 
 
+"""
+for inner testing- cross validation
+cross the data, train the first part and test the second
+"""
+
+
 def cross_validataion(train_x, train_y):
     cut = 3000
     x = train_x[: cut]
@@ -156,6 +227,13 @@ def cross_validataion(train_x, train_y):
     test = train_x[cut:]
     test_y = train_y[cut:]
     return x, y, test, test_y
+
+
+"""
+for inner testing- calc the error rate
+check the y and the y hat for every x and sum the number of mistakes
+divide it by the number of examples
+"""
 
 
 def error_rate(w, test_x, test_y, alg):
@@ -167,29 +245,46 @@ def error_rate(w, test_x, test_y, alg):
     print("Error rate for " + alg + " is:", bad / len(test_y))
 
 
+# print the error rate for every algorithm
 def print_error(w_per, w_svm, w_pa, test_x, test_y):
     error_rate(w_per, test_x, test_y, "perceptron")
     error_rate(w_svm, test_x, test_y, "svm")
     error_rate(w_pa, test_x, test_y, "pa")
 
 
+"""
+get the files -
+open the 3 files, pars them into 3 matrix, 
+normalized thee *_x files and close the files.
+"""
+
+
 def get_files():
-    train_x = parser(sys.argv[1])
-    train_x = normal_data(train_x)  # normalize data
-    file_y = open(sys.argv[2], "r+")
+    train_x = parser(sys.argv[1])  # get train_x
+    train_x = normal_data(train_x)  # normalize train_x
+    file_y = open(sys.argv[2], "r+")  # get train_y
     train_y = [int(float(x)) for x in file_y]
     file_y.close()
-    test_x = parser(sys.argv[3])
-    test_x = normal_data(test_x)  # normalize data
+    test_x = parser(sys.argv[3])  # get test_x
+    test_x = normal_data(test_x)  # normalize test_x
     return train_x, train_y, test_x
+
+
+"""
+open the 3 files.
+train the perceptron, svm and pa algorithms with the given data.
+get the weights matrix for every algorithm
+classify the test_x data with those 3 matrix
+"""
 
 
 def main():
     train_x, train_y, test_x = get_files()
     w_per, w_svm, w_pa = train(train_x, train_y)
     classify(w_per, w_svm, w_pa, test_x)
-    print_error(w_per, w_svm, w_pa, test_x, train_y)
+    # print_error(w_per, w_svm, w_pa, test_x, train_y)
 
 
+# call main
 if __name__ == "__main__":
     main()
